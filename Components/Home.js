cdb94dc52f17ec4577b048d53606877171e3d736
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2022-03-10 08:13:03 +0530

    StartUp commit

---
+++ Components/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from 'react-native'
+const Home = () => {
+    return (
+        <View>
+            <Text>Welcome to ART Work</Text>
+        </View>
+    );
+}
+export default Home
