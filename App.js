e9c1b475a2b7fee3d64a545cf3c86e219bd60f59
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2022-03-21 15:21:35 +0530

    updated icon and splash screen

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import RNBootSplash from "react-native-bootsplash";
 import Todo from './Components/Todo';
 import Progress from './Components/InProgress';
 import Done from './Components/Done';
 const App = () => {
+  useEffect(() => {
+    RNBootSplash.hide(); // fade
+  }, []);
   return (
     <>
       <Todo />
